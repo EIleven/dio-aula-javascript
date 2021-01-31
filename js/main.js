7e68369426b3b8f7a6f66a50afd3f3ccd7c50836
Breno César <44047399+EIleven@users.noreply.github.com>
2021-01-31 04:50:21 -0300

    commit-main-js

---
+++ js/main.js
@@ -0,0 +1,55 @@
+
+var d = new Date();
+alert(d.getHours());
+alert(d)
+
+/*
+var count;
+for(count=0; count <= 5; count++){
+    alert(count);
+}
+*/
+
+
+/*var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+}
+*/
+
+/*
+var idade = prompt("Qual é a sua idade: ");
+if (idade >= 18){
+    alert("Já pode ser preso");
+}else{
+    alert("Menor de idade");
+};
+*/
+
+/* var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}, {nome:"abacate", cor:"verde"}];
+console.log(frutas);
+alert(frutas[1].nome); */
+
+/*var nome = "Breno César";
+var idade = 26;
+var num = 11;
+var frase = "Japão é o melhor";
+var lista = [" maça", " pêra ", " laranja"];
+lista.push(" abacate");
+//lista.pop(), lista.pop()
+console.log(lista);
+alert(lista);
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + num);
+console.log(nome);
+console.log(idade + num);
+console.log(frase.replace("Japão", "Brasil").toLocaleLowerCase());
+alert(frase.replace("melhor","melhor time").toUpperCase());
+console.log(frase.toUpperCase());
+alert(frase.replace("Japão", "Awaí").toLocaleUpperCase());
+alert(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" |")); */
+
